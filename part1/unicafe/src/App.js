228b94d966e3839395f0df6fef2c825a3d0f9055
dennisahlqvist <dennisahlqvist@hotmail.com>
2022-07-30 22:21:17 +0000

    Exercise 1.10

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,6 +1,18 @@
 import { useState } from 'react'
 
+const Button = (props) => {
 
+  return (
+    <button onClick={props.onClick}>
+      {props.text}
+  </button>
+  )
+}
+const StatisticLine = (props) => {
+  return (
+    <div>{props.text} {props.value}</div>
+  )
+}
 
 
 const Statistics = (props) => {
@@ -15,12 +27,12 @@ const Statistics = (props) => {
   return (
     <div>
       <h1>statistics</h1>
-      <div>good {props.good}</div>
-      <div>neutral {props.neutral}</div>
-      <div>bad {props.bad}</div>
-      <div>all {props.all}</div>
-      <div>average {props.average}</div>
-      <div>positive {props.positive} %</div>
+      <StatisticLine text="good" value ={props.good} />
+      <StatisticLine text="neutral" value ={props.neutral} />
+      <StatisticLine text="bad" value ={props.bad} />
+      <StatisticLine text="all" value ={props.all} />
+      <StatisticLine text="average" value ={props.average} />
+      <StatisticLine text="positive" value ={props.positive} />
     </div>
   )
 }
@@ -32,21 +44,19 @@ const App = () => {
   const [bad, setBad] = useState(0)
   const all = good+neutral+bad
   const average = ((good*1)+(neutral*0)+(bad*-1))/all
-  const positive = good/all*100
+  const positive = (good/all*100).toString()+" %"
+  
+  const increaseGood = () => setGood(good + 1)
+  const increaseNeutral = () => setNeutral(neutral + 1)
+  const increaseBad = () => setBad(bad + 1)
 
 
   return (
     <div>
       <h1>give feedback</h1>
-      <button onClick={() => setGood(good + 1)}>
-        good
-      </button>
-      <button onClick={() => setNeutral(neutral + 1)}>
-        neutral
-      </button>
-      <button onClick={() => setBad(bad + 1)}>
-        bad
-      </button>
+      <Button text='good' onClick={increaseGood} />
+      <Button text='neutral' onClick={increaseNeutral} />
+      <Button text='bad' onClick={increaseBad} />
       <Statistics good={good} neutral={neutral} bad={bad} all={all} average={average} positive={positive}/>
 
 
