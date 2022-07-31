5a0309d94b84bf61e9e821f3401931a19440815d
dennisahlqvist <dennisahlqvist@hotmail.com>
2022-07-31 22:35:07 +0000

    Exercise 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 
 const Persons = (props) => {
@@ -25,23 +26,27 @@ const Button = (props) => {
 
 const App = () => {
   
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
-  
-  
-  /*const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' , number: '040-1234567'}
-  ]) */
-  
+  const [persons, setPersons] = useState([])
+    
   const [filteredPersons, setFilteredPersons] = useState(persons)
 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newFilter, setNewFilter] = useState('')
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      //.get('https://3001-dennisahlqv-moocfullsta-d5m513kuso4.ws-eu54.gitpod.io/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+        setFilteredPersons(response.data)
+      })
+  }, [])
+  console.log('render', persons.length, 'persons')
+
 
   const addName = (event) => {
     event.preventDefault()
