57ce926828b0e8de64912eb3462448422769910c
dennisahlqvist <dennisahlqvist@hotmail.com>
2022-08-15 22:54:15 +0300

    Exercise 4.4

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -2,6 +2,13 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  const total = blogs.reduce((total, blog) => {
+    return total + blog.likes
+  }, 0)
+  return total
+}
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
