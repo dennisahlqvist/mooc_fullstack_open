c1530dec8eb96f3b75535b3b477837a9ef795752
dennisahlqvist <dennisahlqvist@hotmail.com>
2022-08-16 19:05:09 +0300

    Exercise 4.2 app

---
+++ part4/blog/app.js
@@ -0,0 +1,15 @@
+//const http = require('http')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const blogsRouter = require('./controllers/blogs')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/blogs', blogsRouter)
+
+module.exports = {app}
--- part4/blog/index.js
+++ part4/blog/index.js
@@ -1,17 +1,10 @@
+const app = require('./app')
 const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-const blogsRouter = require('./controllers/blogs')
+//const express = require('express')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
+const server = http.createServer(app)
 
-app.use(cors())
-app.use(express.json())
-
-app.use('/api/blogs', blogsRouter)
-
-app.listen(config.PORT, () => {
+server.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
 })
