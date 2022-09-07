cc08d63ff6c1c8d1ab52540d09396c2ad66d13b0
dennisahlqvist <dennisahlqvist@hotmail.com>
2022-09-08 01:48:16 +0300

    Moved db connection from model to app

--- part4/blog/app.js
+++ part4/blog/app.js
@@ -13,6 +13,20 @@ const middleware = require('./utils/errorHandler')
 app.use(cors())
 app.use(express.json())
 
+const url = config.MONGODB_URI
+
+logger.info('connecting to', url)
+
+mongoose.connect(url)
+  .then(result => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.info('error connecting to MongoDB:', error.message)
+  })
+
+
+
 app.use('/api/blogs', blogsRouter)
 
 app.use(middleware.errorHandler)
--- part4/blog/models/blog.js
+++ part4/blog/models/blog.js
@@ -1,19 +1,4 @@
-const config = require('../utils/config')
-const logger = require('../utils/logger')
 const mongoose = require('mongoose')
-
-const url = config.MONGODB_URI
-
-logger.info('connecting to', url)
-
-mongoose.connect(url)
-  .then(result => {
-    logger.info('connected to MongoDB')
-  })
-  .catch((error) => {
-    logger.info('error connecting to MongoDB:', error.message)
-  })
-
 
 const blogSchema = new mongoose.Schema({
   title: { type: String, required: true },
